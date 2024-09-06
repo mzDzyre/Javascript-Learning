c82badc89ddbf677ca3364e1430316a90f35a52f
mzDzyre <2003ayriritik@gmail.com>
2024-09-06 18:29:18 +0530

    datatypes basics done

---
+++ 02_datatypes.js
@@ -0,0 +1,23 @@
+// Data Types in javascript
+
+let name = "Ritik"
+let age = 23
+let isLoggedIn = true
+
+/*
+    number => 2 to power 53
+    string => ""
+    bigint
+    boolean => true/false
+    null => standalone value
+    undefined => undefined
+    symbol => unique
+
+
+    object
+*/
+
+console.log(typeof null)  // type is object of null
+console.log(typeof undefined)    // type is undefined selfly
+
+// But the type type of null is tricky because the compiler shows the type is object
