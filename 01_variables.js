cee74b394f3585c6599ab2a5ff59b758342e8377
mzDzyre <2003ayriritik@gmail.com>
2024-09-06 17:50:58 +0530

    variables core concept is done

---
+++ 01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 123456
+let accountEmail = "ritik@google.com"
+var accountName = "Ritik"
+accountType = "Current"
+
+accountEmail = 123
+
+console.table([accountEmail, accountId, accountName, accountType])
